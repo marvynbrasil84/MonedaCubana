d4e4d9a84806a0e7a1d94672e0291f7bb01af4da
marvyn <mamarquez@uci.cu>
2018-05-16 14:03:51 -0400

    Haciendo el dataTable en el js de profesores. Poniendo id a la tabla en el cshtml

--- MonedaCubana/res/jsProfesor.js
+++ MonedaCubana/res/jsProfesor.js
@@ -160,3 +160,77 @@ $("#btnEliminar").click(function () {
         console.log(ex.message);
     }
 });
+
+$(document).ready(function () {
+    Visualizar();
+    Actualizar_Fecha();
+});
+
+function Visualizar(/*pObjet*/) {
+    try {
+        //var _dataSet = new Array();
+        //var aux = JSON.parse(JSON.stringify(pObjet));
+        //if (pObjet != '') {
+        //    $.each(aux, function (idx, obj) {
+
+        //        Aprobar = "<i class= 'fa fa-square-o sel iconh' > </i>";
+
+        //        //_dataSet[idx] = [Aprobar, obj.ACTCOD, obj.ACTDPT, obj.ACTOB1, obj.ACTMAR, obj.ACTMOD, obj.ACTSER, obj.ACTREF, obj.ACTAMT, obj.ACTSEC, obj.ACTCTR, obj.ACTACC];
+        //    });
+        //}
+
+
+        $("#Profesores").dataTable({
+            //"data": _dataSet,
+            //"bProcessing": true,
+            //"sScrollY": 250,
+            "sScrollX": true,
+            //"bInfo": true,
+            //"sInfoEmpty": false,
+            //"bFilter": false,
+            //"bSort": null,
+            //"bAutoCss": true,
+            //"bCollapse": true,
+            //"bInfinite": true,
+            //"bDestroy": true,
+            //"bAutoWidth": false,
+            //"pagingType": "simple",
+            "language": {
+                "url": "dataTables.german.lang",
+                "decimal": ".",
+                "thousands": ","
+            },
+            "oLanguage": {
+
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast": "Último",
+                    "sNext": "Siguiente >>",
+                    "sPrevious": "<< Anterior"
+                }
+            },
+            "oSearch": {
+                "sSearch": "",
+                "bRegex": false,
+                "bSmart": true
+            }
+        });
+
+    }
+    catch (ex) {
+        console.log(ex.message);
+    }
+
+}
